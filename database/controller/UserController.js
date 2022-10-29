0c468c8051d49f3a220e2e3b9bec15bb23f5c1f5
julio cesar <jcmcf2012@gmail.com>
2022-10-29 10:22:52 -0300

    feat: add read user

--- database/controller/UserController.js
+++ database/controller/UserController.js
@@ -1,4 +1,16 @@
 import {response} from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({path: 'config.env'});
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.PASSWORD}@cluster0.pg3doss.mongodb.net/IdeiaOriginal?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log('CONNECTED TO DATABASE');
+  });
+
 import User from '../schemas/User.js';
 class UserController {
   async create(req, res) {
@@ -13,14 +25,23 @@ class UserController {
       return res.json(user);
     } catch (err) {
       return res.status(500).send({
-        error: 'Registration Failed',
+        error: 'User Registration Failed',
         message: err,
       });
     }
   }
 
   async get(req, res) {
-    res.send('Reached');
+    try {
+      const {name} = req.body;
+      const user = await User.find({name: name});
+      return res.json(user);
+    } catch (err) {
+      return res.status(500).send({
+        error: 'Trying to find user failed',
+        message: err,
+      });
+    }
   }
 }
 
