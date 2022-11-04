041948ba51a19333eb29a8b4da359b453e1fa03e
julio cesar <jcmcf2012@gmail.com>
2022-11-04 10:32:47 -0300

    feat: add categories route

--- routes.js
+++ routes.js
@@ -1,9 +1,18 @@
 import UserController from './database/controller/UserController.js';
+import CategoriesController from './database/controller/CategoriesController.js';
 
 import {Router} from 'express';
+
 const routes = Router();
 
 routes.get('/user', UserController.get);
 routes.post('/user', UserController.create);
+routes.delete('/user', UserController.delete);
+routes.patch('/user', UserController.update);
+
+routes.get('/category', CategoriesController.get);
+routes.post('/category', CategoriesController.create);
+routes.delete('/category', CategoriesController.delete);
+routes.patch('/category', CategoriesController.update);
 
 export default routes;
