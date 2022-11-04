7e43c6be9302a9d5e65caf4e5fb62c4d1d104e26
julio cesar <jcmcf2012@gmail.com>
2022-11-04 10:34:41 -0300

    feat: add category model

---
+++ database/schemas/Category.js
@@ -0,0 +1,24 @@
+import {mongoose, Mongoose} from 'mongoose';
+
+const Category = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default Mongoose.models?.Category ||
+  mongoose.model('Category', Category);
